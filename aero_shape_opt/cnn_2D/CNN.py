e18e1f9bb6d6933912b6e8c5c2eb13f51069c19b
ahb341 <36087442+ahb341@users.noreply.github.com>
2021-03-10 01:25:56 -0500

    update cnn

--- aero_shape_opt/cnn_2D/CNN.py
+++ aero_shape_opt/cnn_2D/CNN.py
@@ -6,30 +6,54 @@ from tensorflow import keras
 from tensorflow.keras import layers
 print(tf.__version__)
 
+def loadData(path):
+   with np.load(path,allow_pickle=True) as data:
+      x_train = data['x_train']
+      y_train = data['y_train']
+      x_test = data['x_test']
+      y_test = data['y_test']
+   return x_train,y_train,x_test,y_test
+   # data = np.load(path,allow_pickle=True)
+   # inp = data['input']
+   # out = data['output']
+   # return inp,out
+
 # CNN Model
 def generateModel():
-   model = keras.Sequential([
-      layers.Input(shape=(64,64,1)),
-      # Feature Learning
-      # First convolutional layer
-      layers.Conv2D(32,3,padding='valid',activation='relu'),
-      layers.MaxPooling2D(pool_size=(2,2)),
+   # define two sets of inputs
+   inputA = layers.Input(shape=(28,140,1))
+   inputB = layers.Input(shape=(3,))
+   # first convolutional layer
+   x = layers.Conv2D(32,3,padding='valid',activation='relu')(inputA)
+   x = layers.MaxPooling2D(pool_size=(2,2))(x)
 
-      # Second convolutional layer
-      layers.Conv2D(64,3,activation='relu'),
-      layers.MaxPooling2D(pool_size=(2,2)),
+   # second convolutional layer
+   x = layers.Conv2D(64,3,activation='relu')(x)
+   x = layers.MaxPooling2D(pool_size=(2,2))(x)
 
-      # Third convolutional layer
-      # layers.Conv2D(128,3,activation='relu'),
-      # layers.MaxPooling2D(pool_size=(2,2)),
+   # convolutional model
+   x = layers.Flatten()(x)
+   x = keras.Model(inputs=inputA, outputs=x)
 
-      # Fully connected regression
-      layers.Flatten(),
-      layers.Dense(1024,activation='relu'),
-      layers.Dense(64,activation='relu'),
-      layers.Dense(3) # 3 outputs: cl, cd, cm
-   ])
+   # fully connected regression
+   combined_input = layers.concatenate([x.output,inputB])
+   y = layers.Dense(1024,activation='relu')(combined_input)
+   y = layers.Dense(64, activation="relu")(y)
+   y = layers.Dense(3, activation="relu")(y)
+   y = keras.Model(inputs=[inputA,inputB], outputs=y)
+   # combine the output of the two branches
+   #combined = layers.concatenate([x.output, y.output])
+  
+   # # combined outputs
+   # z = layers.Dense(2, activation="relu")(combined)
+   # z = layers.Dense(1, activation="linear")(z)
+   # # our model will accept the inputs of the two branches and
+   # # then output a single value
+   model = keras.Model(inputs=[x.input, y.input], outputs=y)
    return model
+
+path = 'aero_shape_opt\\datasets\\data_file_cnn.npz'
+xtrain,ytrain,xtest,ytest = loadData(path)
 
 model = generateModel()
 
